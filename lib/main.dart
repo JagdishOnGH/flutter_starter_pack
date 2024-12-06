1685de427dd8d20d9056999d01529dbbd6fd53ab
jagdish bhai <jagdissghimire@gmail.com>
2024-12-06 20:37:28 +0545

    chore:  app router added

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:khurapati_ideas/pages/welcome_page.dart';
+import 'package:khurapati_ideas/core/routes/app_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final _appRouter = AppRouter();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: _appRouter.config(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: WelcomePage(),
     );
   }
 }

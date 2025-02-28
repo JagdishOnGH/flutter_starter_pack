4f7c4c01eaaa45ea407b9f000c7503d6e3ea9923
jagdish bhai <jagdissghimire@gmail.com>
2025-02-28 10:18:06 +0545

    chore: input decoration added

---
+++ lib/core/theme/app_input_decoration.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final appLightInputDecoration = InputDecorationTheme(
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10),
+  ),
+);

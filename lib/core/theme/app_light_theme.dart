bee1a72e4f6d2a98e7a2e01170d19cc502f14fa2
jagdish bhai <jagdissghimire@gmail.com>
2025-02-28 10:18:22 +0545

    chore: input decoration added

--- lib/core/theme/app_light_theme.dart
+++ lib/core/theme/app_light_theme.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:khurapati_ideas/core/theme/app_input_decoration.dart';
 
-ThemeData AppLightTheme = ThemeData();
+ThemeData AppLightTheme =
+    ThemeData(inputDecorationTheme: appLightInputDecoration);
